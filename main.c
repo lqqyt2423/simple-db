4b88e33736ca53f8780694d5508159080839d8db
liqiang <gymbo@gymbodeMacBook-Pro.local>
2020-11-03 16:03:07 +0800

    Part 8 - B-Tree Leaf Node Format

--- main.c
+++ main.c
@@ -235,6 +235,28 @@ Table* db_open(const char* filename) {
 	return table;
 }
 
+void leaf_node_insert(Cursor* cursor, uint32_t key, Row* value) {
+	void* node = get_page(cursor->table->pager, cursor->page_num);
+
+	uint32_t num_cells = *leaf_node_num_cells(node);
+	if (num_cells >= LEAF_NODE_MAX_CELLS) {
+		// Node full
+		printf("Need to implement splitting a leaf node.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (cursor->cell_num < num_cells) {
+		// Make room for new cell
+		for (uint32_t i = num_cells; i > cursor->cell_num; i--) {
+			memcpy(leaf_node_cell(node, i), leaf_node_cell(node, i - 1), LEAF_NODE_CELL_SIZE);
+		}
+	}
+
+	*(leaf_node_num_cells(node)) += 1;
+	*(leaf_node_key(node, cursor->cell_num)) = key;
+	serialize_row(value, leaf_node_value(node, cursor->cell_num));
+}
+
 void db_close(Table* table) {
 	Pager* pager = table->pager;
 
@@ -318,10 +340,36 @@ InputBuffer* new_input_buffer() {
 	return input_buffer;
 }
 
+void print_constants() {
+	printf("ROW_SIZE: %d\n", ROW_SIZE);
+	printf("COMMON_NODE_HEADER_SIZE: %d\n", COMMON_NODE_HEADER_SIZE);
+	printf("LEAF_NODE_HEADER_SIZE: %d\n", LEAF_NODE_HEADER_SIZE);
+	printf("LEAF_NODE_CELL_SIZE: %d\n", LEAF_NODE_CELL_SIZE);
+	printf("LEAF_NODE_SPACE_FOR_CELLS: %d\n", LEAF_NODE_SPACE_FOR_CELLS);
+	printf("LEAF_NODE_MAX_CELLS: %d\n", LEAF_NODE_MAX_CELLS);
+}
+
+void printf_leaf_node(void* node) {
+	uint32_t num_cells = *leaf_node_num_cells(node);
+	printf("leaf (size %d)\n", num_cells);
+	for (uint32_t i = 0; i < num_cells; i++) {
+		uint32_t key = *leaf_node_key(node, i);
+		printf("  - %d : %d\n", i, key);
+	}
+}
+
 MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table* table) {
 	if (strcmp(input_buffer->buffer, ".exit") == 0) {
 		db_close(table);
 		exit(EXIT_SUCCESS);
+	} else if (strcmp(input_buffer->buffer, ".btree") == 0) {
+		printf("Tree:\n");
+		printf_leaf_node(get_page(table->pager, 0));
+		return META_COMMAND_SUCCESS;
+	} else if (strcmp(input_buffer->buffer, ".constants") == 0) {
+		printf("Constants:\n");
+		print_constants();
+		return META_COMMAND_SUCCESS;
 	} else {
 		return META_COMMAND_UNRECOGNIZED_COMMAND;
 	}
@@ -370,16 +418,15 @@ PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
 }
 
 ExecuteResult execute_insert(Statement *statement, Table* table) {
-	if (table->num_rows >= TABLE_MAX_ROWS) {
+	void* node = get_page(table->pager, table->root_page_num);
+	if ((*leaf_node_num_cells(node) >= LEAF_NODE_MAX_CELLS)) {
 		return EXECUTE_TABLE_FULL;
 	}
 
 	Row* row_to_insert = &(statement->row_to_insert);
 	Cursor* cursor = table_end(table);
 
-	serialize_row(row_to_insert, cursor_value(cursor));
-	table->num_rows += 1;
-
+	leaf_node_insert(cursor, row_to_insert->id, row_to_insert);
 	free(cursor);
 
 	return EXECUTE_SUCCESS;
